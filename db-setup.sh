43456172c8685467d4222f2f6271404817977808
alpanachaphalkar <alpana.chaphalkar@sky.de>
2022-06-09 07:45:09 +0200

    Modified db-setup.sh

--- db-setup.sh
+++ db-setup.sh
@@ -1,3 +1,7 @@
+docker pull alpine/git:latest
+docker pull busybox:latest
+docker pull postgres:9.6
+
 kubectl create ns psc-db
 
 cd "C:\Users\ALCH390\GolandProjects\TIBCO-PSC-DB\postgresql\9.6\helm"
