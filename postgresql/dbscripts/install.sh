e45a568f11601adc701a233df4cab107a25f7116
alpanachaphalkar <alpana.chaphalkar@sky.de>
2022-06-26 01:15:09 +0200

    testing db-installation script.

--- postgresql/dbscripts/install.sh
+++ postgresql/dbscripts/install.sh
@@ -19,68 +19,68 @@ echo "--------------------------------------------------------------------------
 # Launch the mdm scripts
 echo "-------------------------- Launching MDM Scripts ---------------------------"
 "${PGSQL}" -v mdm_data_tbs_storage="${MDMDBDATATBSSTORAGE}" -v mdm_data_index_storage="${MDMDBINDEXTBSSTORAGE}" -f ./mdm/configure/create_tablespaces.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -v mdm_user="${MDMDBUSER}" -v mdm_password="'${MDMDBPASSWORD}'" -f ./mdm/configure/create_users.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -v mdm_db="${MDMINSTANCENAME}" -f ./mdm/configure/create_database.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -v mdm_db_schema="${MDMDBSCHEMA}" -v pg_super_user="${PGUSER}" -v mdm_user="${MDMDBUSER}" -f ./mdm/configure/create_schema.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/configure/create_extentions.sql
+"${PGSQL}" -v mdm_user="${MDMDBUSER}" -v mdm_password="'${MDMDBPASSWORD}'" -f ./mdm/configure/create_users.sql
+"${PGSQL}" -v mdm_db="${MDMINSTANCENAME}" -f ./mdm/configure/create_database.sql
+"${PGSQL}" -v mdm_db_schema="${MDMDBSCHEMA}" -v pg_super_user="${PGUSER}" -v mdm_user="${MDMDBUSER}" -f ./mdm/configure/create_schema.sql
+"${PGSQL}" -f ./mdm/configure/create_extentions.sql
 
 export PGUSER="${MDMDBUSER}"
 export PGPASSWORD="${MDMDBPASSWORD}"
 export PGDATABASE="${MDMINSTANCENAME}"
 
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -v mdm_user="${MDMDBUSER}" -v mdm_db_schema="${MDMDBSCHEMA}" -f ./mdm/configure/alter_searchpath.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/ddl/create_sequeces.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/ddl/create_tables.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/ddl/create_PK.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/ENTERPRISE.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/ORGANIZATION.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/DOMAIN.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/DOMAINENTRY.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/RELEASE.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/RESOURCEACCESS.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/CATALOGATTRIBUTEDATATYPE.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/CONFIGURATIONDEFINITION.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/SCREEN.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/FUNCTION.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/HTMLELEMENTID.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/FUNC2IDMAP.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/FUNC2SCREENMAP.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/ROLE.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/ROLE2FUNCMAP.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/SUPPLIERSTATE.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/WORKFLOWFORM.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/MEMBER.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/MEMBERSHIP.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/OBJECTSEQUENCE.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/ROLEASSIGNMENT.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/DOMAINLINK.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/DOMAINVALUE.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/ATTRIBUTEGROUP.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/CATALOGFORMAT.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/CATALOGTYPE.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/CATALOGTYPEATTRIBUTE.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/CATALOG.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/CATALOGATTRIBUTE.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/RELATIONSHIP.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/PROCESSSELECTION.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/seed/HIVALUE.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/ddl/create_indexes.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/ddl/create_FK.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/ddl/create_views.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/ddl/create_functions.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./mdm/install/scripts/triggers/create_triggers.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -v mdm_db_instance_name="${MDMINSTANCENAME}" -v mdm_db_instance_desc="${MDMINSTANCEDESCRIPTION}" -f ./mdm/MDMInstance.sql
+"${PGSQL}" -v mdm_user="${MDMDBUSER}" -v mdm_db_schema="${MDMDBSCHEMA}" -f ./mdm/configure/alter_searchpath.sql
+"${PGSQL}" -f ./mdm/install/scripts/ddl/create_sequeces.sql
+"${PGSQL}" -f ./mdm/install/scripts/ddl/create_tables.sql
+"${PGSQL}" -f ./mdm/install/scripts/ddl/create_PK.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/ENTERPRISE.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/ORGANIZATION.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/DOMAIN.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/DOMAINENTRY.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/RELEASE.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/RESOURCEACCESS.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/CATALOGATTRIBUTEDATATYPE.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/CONFIGURATIONDEFINITION.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/SCREEN.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/FUNCTION.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/HTMLELEMENTID.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/FUNC2IDMAP.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/FUNC2SCREENMAP.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/ROLE.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/ROLE2FUNCMAP.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/SUPPLIERSTATE.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/WORKFLOWFORM.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/MEMBER.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/MEMBERSHIP.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/OBJECTSEQUENCE.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/ROLEASSIGNMENT.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/DOMAINLINK.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/DOMAINVALUE.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/ATTRIBUTEGROUP.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/CATALOGFORMAT.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/CATALOGTYPE.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/CATALOGTYPEATTRIBUTE.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/CATALOG.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/CATALOGATTRIBUTE.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/RELATIONSHIP.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/PROCESSSELECTION.sql
+"${PGSQL}" -f ./mdm/install/scripts/seed/HIVALUE.sql
+"${PGSQL}" -f ./mdm/install/scripts/ddl/create_indexes.sql
+"${PGSQL}" -f ./mdm/install/scripts/ddl/create_FK.sql
+"${PGSQL}" -f ./mdm/install/scripts/ddl/create_views.sql
+"${PGSQL}" -f ./mdm/install/scripts/ddl/create_functions.sql
+"${PGSQL}" -f ./mdm/install/scripts/triggers/create_triggers.sql
+"${PGSQL}" -v mdm_db_instance_name="${MDMINSTANCENAME}" -v mdm_db_instance_desc="${MDMINSTANCEDESCRIPTION}" -f ./mdm/MDMInstance.sql
 echo "-------------------------- Finished MDM Scripts Execution ---------------------------"
 
 # Launch the fc scripts
 echo "------------------------------ Launching FC Scripts -------------------------------"
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./fc/install/scripts/seed/function_ac.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./fc/install/scripts/seed/SCREEN.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./fc/install/scripts/seed/FC_DEL_FROM_DBE_RELATIONSHIP.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./fc/install/scripts/seed/FC_GET_DELETED_RELATIONSHIP.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./fc/install/scripts/seed/FC_TU_PROCESS.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./fc/install/scripts/seed/FC_UPDATELIFECYCLESTATUS.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./fc/utility/ClearNamedversionData.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./fc/install/scripts/seed/FC_GET_REL_DEL_MOD_IN_TS.sql
-"${PGSQL}" -U "${PGUSER}" -d "${PGDATABASE}" -f ./fc/install/scripts/seed/create_tabs.sql
+"${PGSQL}" -f ./fc/install/scripts/seed/function_ac.sql
+"${PGSQL}" -f ./fc/install/scripts/seed/SCREEN.sql
+"${PGSQL}" -f ./fc/install/scripts/seed/FC_DEL_FROM_DBE_RELATIONSHIP.sql
+"${PGSQL}" -f ./fc/install/scripts/seed/FC_GET_DELETED_RELATIONSHIP.sql
+"${PGSQL}" -f ./fc/install/scripts/seed/FC_TU_PROCESS.sql
+"${PGSQL}" -f ./fc/install/scripts/seed/FC_UPDATELIFECYCLESTATUS.sql
+"${PGSQL}" -f ./fc/utility/ClearNamedversionData.sql
+"${PGSQL}" -f ./fc/install/scripts/seed/FC_GET_REL_DEL_MOD_IN_TS.sql
+"${PGSQL}" -f ./fc/install/scripts/seed/create_tabs.sql
 echo "-------------------------- Finished FC Scripts Execution ---------------------------"
